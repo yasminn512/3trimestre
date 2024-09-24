6a265205a4d446ead075f48b6cc011d2b81b1608
yasmin <114156166+yasminn512@users.noreply.github.com>
2024-09-24 11:58:12 -0300

    ok

---
+++  script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:"pergunta 1",
+        alternativas:[
+        "alternativa 1 ",
+        "alternativa 2 " 
+        ]
+    }
+]{
+
+}
+V
+ {
+[;
+
+]
+ }
