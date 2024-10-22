86166ee4580469ee01b0289c745811cfe39fcdc0
yasmin <114156166+yasminn512@users.noreply.github.com>
2024-10-22 12:13:55 -0300

    ok

---
+++ redesFavoritasMundo.js
@@ -0,0 +1,38 @@
+import { getCSS , criargrafico } from "./common";
+
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontet.com/guilhermeonrails/api/main/
+    redes-redesFavoritasMundo.json'
+    const res = await fetch(url)
+    const dados= awaitres.json()
+    const redes = Object.values(dados)
+}
+const data =[
+    {
+      values:valores,
+      labels: redes,
+      type:'pie',
+      textinfo:'label+percent'  
+    }
+]
+ 
+constlayout= {
+    plot_bgcolor:getCSS('--bg-color')
+    peper_bgcolor:getCSS('--bg-color')
+    height:700,
+    title:{
+        text: 'redes sociais que oos usúarios mais gostam',
+        x:0,
+        font:{
+            color:getCSS('--primary-color'),
+            family:getCSS('--font'),
+            size:30
+        }
+    }
+},
+legend:{
+    font:{
+        color:getCSS('--primary-color'),
+        size:16
+    }
+}
